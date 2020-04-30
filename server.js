54f60ea654d172e7d3d4e2e71a9b693fcce71ead
abhayzzz <abhayraut712@gmail.com>
2020-05-01 01:35:10 +0545

    created a website front end database works remain

---
+++ server.js
@@ -0,0 +1,47 @@
+"user strict";
+var express = require('express');
+var app = express();
+var developernamedefault = "Abhay Raut";
+var developercountrydefault = "Nepal";
+var bodyParser = require('body-parser');
+var path = require('path');
+
+//create handlebars with default layout
+var handlebars = require('express-handlebars')
+.create({defaultLayout:'main'});
+
+app.engine('handlebars',handlebars.engine);
+app.set('view engine','handlebars');
+
+app.set('port',process.env.PORT || 5000);
+app.use(express.static(path.resolve(__dirname,'public')));
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.get('/',function(req,res){
+    res.render('home',{
+        layout:'main',
+        developername : developernamedefault,
+        developercountry : developercountrydefault
+    });
+});
+app.post('/contact',function(req,res){
+        console.log(req.body);  //prints all the form contents to console log we can use this data to store in data base
+        res.render('contact');
+    });
+
+//need declare handle error http here (after get post put actions etc)
+//404 not found
+app.use(function(req,res){
+    res.status(404);
+    res.render('404');
+});
+//500 server error
+app.use(function(err,req,res,next){
+    console.error(err.stack);
+    res.status(500);
+    res.render('500');
+});
+
+app.listen(app.get('port'),function(){
+    console.log('express started on http://localhost:'+app.get('port'));
+})
